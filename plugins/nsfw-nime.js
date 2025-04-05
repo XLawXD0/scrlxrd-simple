a781cb6ca645906a1999b01adc8477ddfd22086c
XLaw XD <101106351+XLawXD0@users.noreply.github.com>
2025-04-05 22:09:48 +0700

    ScrLxrd Simple

--- plugins/nsfw-nime.js
+++ plugins/nsfw-nime.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 let handler = async (m, { conn, usedPrefix, command }) => {
 	
-	if (!global.db.data.chats[m.chat].nsfw) throw `🚫 ${mssg.gpNsfw(usedPrefix)}`
+	/*if (!global.db.data.chats[m.chat].nsfw) throw `🚫 ${mssg.gpNsfw(usedPrefix)}`*/
    let user = global.db.data.users[m.sender].age
    if (user < 17) throw `❎ ${mssg.nsfwAge}`
    
